369254402e4ed96fd1fd301d5f12f11a7a398256
Isaac <siphron0@gmail.com>
2021-04-30 17:50:11 +0200

    [UPD] Fighting methods

--- src/Fight.ts
+++ src/Fight.ts
@@ -2,21 +2,45 @@ import {Pokemon} from "./pokemon";
 import {FirstToAttack} from "./index";
 
 export class Fight{
-    async fightOfTwoPokemons(pokemon1: Pokemon, pokemon2: Pokemon): Pokemon | null {
 
-    if(pokemon1.health <= 0 || pokemon2.health <= 0){
-        return null;
-    }else{
-        const firstToAttack = FirstToAttack(pokemon1,pokemon2);
-        while (pokemon1.isAlive() && pokemon2.isAlive()){
+    firstPokemon: Pokemon;
+    secondPokemon: Pokemon;
+
+    constructor(firstPokemon: Pokemon, secondPokemon: Pokemon) {
+        this.firstPokemon = firstPokemon;
+        this.secondPokemon = secondPokemon;
+    }
+
+    decideWinner(): Pokemon{
+        return !this.firstPokemon.isAlive() ? this.firstPokemon : this.secondPokemon;
+    }
+
+    async Fighting(firstPokemon: Pokemon, secondPokemon: Pokemon): Promise<Promise<Pokemon> | null> {
+
+
+        const firstPokemonToAttack = FirstToAttack(firstPokemon,secondPokemon);
+        const opponent:Pokemon = firstPokemonToAttack == firstPokemon ? secondPokemon : secondPokemon;
+
+        while (firstPokemon.isAlive() && secondPokemon.isAlive()){
             setTimeout(() => {
-                
+                firstPokemonToAttack.basicAttack(opponent);
+            }, 1000);
+            setTimeout(() => {
+                opponent.basicAttack(firstPokemonToAttack);
             }, 1000);
         }
 
-    }
+        return null;
+
 
     }
+
+   /* async startFight(): Promise<Pokemon> | null{
+        if(
+        Fight();
+    }*/
+
+
 }
 
 
